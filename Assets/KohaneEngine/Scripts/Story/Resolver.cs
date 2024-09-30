bb6fcd3d8f86ddb5434ee2bfaf68239ffb6de5f5
totorowldox <2553658033@qq.com>
2024-09-30 22:28:44 +0900

    refactor: script resolvers

--- Assets/KohaneEngine/Scripts/Story/Resolver.cs
+++ Assets/KohaneEngine/Scripts/Story/Resolver.cs
@@ -1,11 +1,18 @@
-﻿using KohaneEngine.Scripts.Structure;
+﻿using System;
+using System.Collections.Generic;
+using KohaneEngine.Scripts.Structure;
 
 namespace KohaneEngine.Scripts.Story
 {
     public abstract class Resolver
     {
-        public static Resolver Instance;
+        protected readonly Dictionary<string, Func<Block, ResolveResult>> Functions = new();
 
-        public abstract ResolveResult Resolve(Block block);
+        public ResolveResult Resolve(Block block)
+        {
+            return Functions.TryGetValue(block.type, out var function)
+                ? function.Invoke(block)
+                : ResolveResult.FailResult($"[GenericResolver] Unknown block type {block.type}");
+        }
     }
 }
--- Assets/KohaneEngine/Scripts/Story/Resolvers/BackgroundResolver.cs
+++ Assets/KohaneEngine/Scripts/Story/Resolvers/BackgroundResolver.cs
@@ -20,44 +20,56 @@ namespace KohaneEngine.Scripts.Story.Resolvers
             _resourceManager = resourceManager;
             _backgroundImage = binder.backgroundImage;
             _animator = animator;
+            Functions.Add("__bgSwitch", BgSwitch);
+            Functions.Add("__bgRemove", BgRemove);
+            Functions.Add("bgMove", BgMove);
+            Functions.Add("bgScale", BgScale);
+            Functions.Add("bgAlpha", BgAlpha);
         }
-        
-        public override ResolveResult Resolve(Block block)
+
+        private ResolveResult BgScale(Block block)
         {
-            switch (block.type)
-            {
-                case "__bgSwitch":
-                    _animator.AppendCallback(() => SetBackgroundImage(block.GetArg<string>(0)));
-                    break;
-                case "__bgRemove":
-                    _backgroundImage.sprite = null;
-                    break;
-                case "bgAlpha":
-                    var alpha = block.GetArg<float>(0);
-                    var tween = block.GetArg<int>(1);
-                    var dur = block.GetArg<float>(2);
-                    _animator.AppendAnimation(_backgroundImage
-                        .DOFade(alpha, dur).SetEase((Ease) tween));
-                    break;
-                case "bgMove":
-                    var ax = block.GetArg<float>(0);
-                    var ay = block.GetArg<float>(1);
-                    tween = block.GetArg<int>(2);
-                    dur = block.GetArg<float>(3);
-                    _animator.AppendAnimation(_backgroundImage.rectTransform
-                        .DOAnchorPos(UIUtils.ScriptPositionToCanvasPosition(new Vector2(ax, ay)), dur)
-                        .SetEase((Ease) tween));
-                    break;
-                case "bgScale":
-                    ax = block.GetArg<float>(0);
-                    ay = block.GetArg<float>(1);
-                    tween = block.GetArg<int>(2);
-                    dur = block.GetArg<float>(3);
-                    _animator.AppendAnimation(_backgroundImage.rectTransform
-                        .DOScale(new Vector3(ax, ay, 1), dur)
-                        .SetEase((Ease) tween));
-                    break;
-            }
+            var ax = block.GetArg<float>(0);
+            var ay = block.GetArg<float>(1);
+            var tween = block.GetArg<int>(2);
+            var dur = block.GetArg<float>(3);
+            _animator.AppendAnimation(_backgroundImage.rectTransform
+                .DOScale(new Vector3(ax, ay, 1), dur)
+                .SetEase((Ease) tween));
+            return ResolveResult.SuccessResult();
+        }
+
+        private ResolveResult BgMove(Block block)
+        {
+            var ax = block.GetArg<float>(0);
+            var ay = block.GetArg<float>(1);
+            var tween = block.GetArg<int>(2);
+            var dur = block.GetArg<float>(3);
+            _animator.AppendAnimation(_backgroundImage.rectTransform
+                .DOAnchorPos(UIUtils.ScriptPositionToCanvasPosition(new Vector2(ax, ay)), dur)
+                .SetEase((Ease) tween));
+            return ResolveResult.SuccessResult();
+        }
+
+        private ResolveResult BgAlpha(Block block)
+        {
+            var alpha = block.GetArg<float>(0);
+            var tween = block.GetArg<int>(1);
+            var dur = block.GetArg<float>(2);
+            _animator.AppendAnimation(_backgroundImage
+                .DOFade(alpha, dur).SetEase((Ease) tween));
+            return ResolveResult.SuccessResult();
+        }
+
+        private ResolveResult BgRemove(Block block)
+        {
+            _backgroundImage.sprite = null;
+            return ResolveResult.SuccessResult();
+        }
+
+        private ResolveResult BgSwitch(Block block)
+        {
+            _animator.AppendCallback(() => SetBackgroundImage(block.GetArg<string>(0)));
             return ResolveResult.SuccessResult();
         }
 
